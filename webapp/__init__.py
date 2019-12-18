ba2e73c29fa783cf1c59081cecfe478ae3dbae48
Serebrennikov <smit7bb@mail.ru>
2019-12-18 19:48:19 +0500

    Made code refactoring

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template
 from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
 
-app = Flask(__name__)
+    @app.route('/')
+    def index():
+        title = "Новости Python"
+        weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
+        news = get_python_news()
+        return render_template('index.html', page_title=title, weather=weather, news_lists=news)
 
-
-@app.route('/')
-def index():
-    title = "Новости Python"
-    weather = weather_by_city('Yekaterinburg,Russia')
-    news = get_python_news()
-    return render_template('index.html', page_title=title, weather=weather, news_lists=news)
+    return app
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    create_app().run(debug=True)
--- webapp/weather.py
+++ webapp/weather.py
@@ -1,10 +1,11 @@
+from flask import current_app
 import requests
-import weather_settings
+
 
 def weather_by_city(city_name):
-    weather_url = weather_settings.weather_url
+    weather_url = current_app.config['WEATHER_URL']
     params = {
-        'key': weather_settings.API_KEY,
+        'key': current_app.config['WEATHER_API_KEY'],
         'q': city_name,
         'format': 'json',
         'num_of_days': 1,

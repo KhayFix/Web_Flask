40843526601e742772adabcf0ececd216290516a
Serebrennikov <smit7bb@mail.ru>
2020-01-14 19:37:21 +0500

    Активировал миграции. В блюпринте  user/models создал email для пользователя

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
-
+from flask_migrate import Migrate
 from webapp.db import db
 from webapp.admin.views import blueprint as admin_blueprint
 from webapp.news.views import blueprint as news_blueprint
@@ -12,11 +12,14 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    # включаем работу с миграциями
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
     # указываем название нашей функции def login()
     login_manager.login_view = "user.login"
+
     # Подключаем блюпринт пользователя и админа
     app.register_blueprint(admin_blueprint)
     app.register_blueprint(user_blueprint)

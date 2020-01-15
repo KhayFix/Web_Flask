bdf9254971da15fd15eb616c90a1a11a1b5c9097
Serebrennikov <smit7bb@mail.ru>
2020-01-15 17:05:22 +0500

    Сделано Flask-Migrations и регистрация пользователей

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, flash, render_template, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 
-from webapp.user.forms import LoginForm
+from webapp.db import db
+from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
 
 # т.к. у нас блюприн, импортировать app сюда не будем. Будем использовать Blueprint.
@@ -39,3 +40,31 @@ def logout():
     logout_user()
     flash('Вы успешно вышли')
     return redirect(url_for('news.index'))
+
+# роут отвечающий за регистрацию пользователя
+@blueprint.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('news.index'))
+    title = "Регистрация"
+    regist_form = RegistrationForm()
+    return render_template('user/registration.html', page_title=title, form=regist_form)
+
+# Роут отвечающий за процессинг, обработку регистрации
+@blueprint.route('/process-reg', methods=['POST'])
+def process_reg():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        # user = User.query.filter(User.username =! form.username.data).first()
+        # if user and user.check_password(form.password.data):
+        news_user = User(username=form.username.data, email=form.email.data, role='user')
+        news_user.set_password(form.password.data)
+        db.session.add(news_user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!')
+        # перенаправляем пользователя на форму входа на сайт
+        return redirect(url_for('user.login'))
+
+    flash('Пожалуйста исправте ошибки в форме регистрации')
+    return redirect(url_for('user.register'))

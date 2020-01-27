77409f6273b5e88caf29106e154a74015fa77abc
Serebrennikov <smit7bb@mail.ru>
2020-01-27 17:36:22 +0500

    Создана модель Comment, сделано отображение комментариев в конце статьи

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from sqlalchemy.orm import relationship
+
 from webapp.db import db
 
 
@@ -9,5 +12,31 @@ class News(db.Model):
     text = db.Column(db.Text, nullable=True)
     author_published = db.Column(db.String, unique=True, nullable=True)
 
+    def comments_count(self):
+        return Comment.query.filter(Comment.news_id == self.id).count()
+
     def __repr__(self):
-        return '<News {}{}>'.format(self.title, self.url)
+        return '<News {}{} {}>'.format(self.title, self.url, self.author_published)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    news_id = db.Column(
+        db.Integer,
+        db.ForeignKey('news.id', ondelete='CASCADE'),
+        index=True
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
+
+    # relationship добавляет удобный интерфейс доступа к связанным данным
+    news = relationship('News', backref='comments')
+    user = relationship('User', backref='comments')
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
